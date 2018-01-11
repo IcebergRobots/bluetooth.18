9463177decd2e7e7b3da37225ba9fe4a689ceb1d
Anton Pusch <anton.pusch@outlook.de>
2018-01-11 13:37:25 +0100

    Gleiches Programm für MASTER und SLAVE

--- bluetooth.ino
+++ bluetooth.ino
@@ -2,57 +2,40 @@
 #define debug(_str_); if(DEBUG){Serial.print(_str_);}
 #define debugln(_str_); if(DEBUG){Serial.println(_str_);}
 
+#define SEND true
 #define LED 13
-#define BUTTON A8
+#define BUTTON A0
 
 String bluetooth;
-enum ROLE {MASTER, SLAVE};
-ROLE r;
 unsigned long lastMes = 0;
-bool isLed = false;
+bool isButton = false;
+bool lastButton = false;
 
 void setup() {
-  r = SLAVE;   //==============================================00
+  pinMode(LED, OUTPUT);
+  pinMode(BUTTON, INPUT_PULLUP);
   Serial.begin(9600);
-  switch (r)
-  {
-    case MASTER:
-      debugln("RESTART as MASTER");
-      Serial3.begin(38400);
-      Serial3.println("IR:available");
-      break;
-    case SLAVE:
-      debugln("RESTART as SLAVE");
-      Serial3.begin(9600);
-      break;
-    default: break;
-  }
+  debugln("RESTART");
+  Serial3.begin(38400);
+  Serial3.println("IR:available");
 }
 void loop() {
-  switch (r)
-  {
-    case MASTER:
-      if (millis() - lastMes > 2000) {
-        lastMes = millis();
-        analogWrite(LED, 200);
-        delay(10);
-        analogWrite(LED, 0);
-        if (isLed) {
-          char mes[] = "42";
-          Serial3.print(mes);
-        } else {
-          Serial3.print("IR:on");
-        }
-        isLed = !isLed;
-      }
-      delay(100);
-      break;
-    case SLAVE:
-      if (Serial3.available() > 0) {
-        bluetooth = Serial3.readString();
-        Serial.println(bluetooth);
-      }
-      break;
-    default: break;
+  isButton = !digitalRead(BUTTON);
+  if (isButton != lastButton) {
+    if (isButton) {
+      Serial3.write("<set led on>");
+    } else {
+      Serial3.print("<set led off>");
+    }
   }
+  if (Serial3.available() > 0) {
+    bluetooth = Serial3.readString();
+    debugln(bluetooth);
+    if (bluetooth == "<set led on>") {
+      analogWrite(LED, 250);
+    } else if (bluetooth == "<set led off>") {
+      analogWrite(LED, 0);
+    }
+  }
+  lastButton = isButton;
 }
